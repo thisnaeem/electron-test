f4e9661a338537adf93072838dd596b0c9a278cd
Naeem Anjum <naeemanjum076@gmail.com>
2025-12-17 12:17:20 +0500

    Delete src directory

--- src/renderer/src/context/GeminiContext.context.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react'
-import { GeminiContextType } from './GeminiContext.types'
-
-export const GeminiContext = createContext<GeminiContextType | undefined>(undefined)
